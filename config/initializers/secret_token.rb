3e5db5abe36ddaedf3d2d8bb4f08f5524d3f2d55
root <root@bt.foo.org>
2013-07-31 08:00:44 +0700

    Aplikasi AlQuran_reminder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlQuranReminder::Application.config.secret_token = '90edb3d08948397d1feb8e36cbe806671e6c346088de11e30a0e63fd7a86795aa19f071a7f880a0b6a035fa46e4df621bf23f1c93f5ceeee77c08681c26ad58c'
