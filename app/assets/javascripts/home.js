c44d63db89f099ddfd6294cfccde64f98862d83d
root <root@bt.foo.org>
2013-09-04 17:02:15 +0700

    setting reminder

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,18 @@
+function myFunction() {
+	setInterval(function(){
+		window.reminders.forEach(function(x)){
+			if(x.show === false){
+				if(x.remind_every < new Date()){
+					x.show = true
+				}
+			}
+		}
+
+	});
+},1000);
+
+
+$(myFunction);
+
+
+
--- app/assets/javascripts/reminder.js
+++ app/assets/javascripts/reminder.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
+	
 	$("select#reminder_surat_id").on('change', function(e){
 		$.get('/surats/' + e.target.value + '/ayats');
 	});
 	$.get('/reminders/check', function(data){
 		if(data) {
 			$.get('/reminders.js');
+	
+	
 		}
 	});
 });
+
+
+
+
+
+
+ 
